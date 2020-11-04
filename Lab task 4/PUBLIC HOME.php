d19904405cc6046af85bb203e7897b0db82b567a
G.K.Hasan <60335274+hasan722@users.noreply.github.com>
2020-11-04 22:33:34 +0600

    Add files via upload

---
+++ Lab task 4/PUBLIC HOME.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <title>NDC Company</title>
+  <h1 > <img src="HR Company Logo Generator copy.jpg" alt="company" width="100" height="100"> Comapany </h1>
+</head>
+<body> 
+<table align="center">
+
+<?php
+echo "Welcome to NDC Company";
+?>
+<tr>
+<td><a href="REGISTRATION.php"> Registration </a></td>
+<td><a href="LOGIN.php"> Login </a></td>
+<td><a href="PUBLIC HOME.php"> Home </a></td>
+</tr>
+</table>
+<?php include 'footer.php';?>
+ </body>
+</html>
